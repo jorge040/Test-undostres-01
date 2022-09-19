3001742d934decb4cb69a3b0c2c98575f2f209a3
Jorge Ivan Cuellar <46494068+jorge040@users.noreply.github.com>
2022-09-19 16:46:34 -0500

    Test 02
    second code

---
+++ Soy el segundoejercisio con resultado 2.py
@@ -0,0 +1,37 @@
+def swap(nums, i, j):
+	temp = nums[i]
+	nums[i] = nums[j]
+	nums[j] = temp
+
+def partition(nums):
+	pIndex = 0
+
+	for i in range(len(nums)):
+		if nums[i] > 0:		# pivot is 0:
+			swap(nums, i, pIndex)
+			pIndex += 1
+
+	return pIndex
+
+def findSmallestMissing(nums):
+
+	k = partition(nums)
+
+	for i in range(k):
+
+		val = abs(nums[i])
+
+		if val-1 < k and nums[val-1] >= 0:
+			nums[val-1] = -nums[val-1]
+
+	for i in range(k):
+		if nums[i] > 0:
+			return i + 1
+
+	return k + 1
+
+if __name__ == '__main__':
+
+	nums = [3, 4, -1, 1]
+
+	print('The smallest positive missing number is', findSmallestMissing(nums))
